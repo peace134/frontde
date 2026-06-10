848b48f6e6e6b25c751dde36fa6b9c5fbc726d88
peace134 <1939319005@qq.com>
2026-06-10 18:42:08 +0800

    添加 GitHub Actions 自动部署

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+    base: '/frontde/',
     plugins: [vue()],
     resolve: {
         alias: {
