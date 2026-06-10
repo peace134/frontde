f9a271257313ab4352c6bb031cafc234a42b8457
peace134 <1939319005@qq.com>
2026-06-10 18:58:42 +0800

    修复：路由模式改为 Hash 模式以兼容 GitHub Pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from '@/views/Login.vue'
 import Register from '@/views/Register.vue'
 import MainLayout from '@/layouts/MainLayout.vue'
@@ -31,7 +31,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes
 })
 
